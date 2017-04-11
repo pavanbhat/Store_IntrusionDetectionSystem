376aaad75a126494f8a0c746cbcfd15e6194bf94
Pavan <pxb8715@rit.edu>
2017-04-10 22:12:32 -0400

    queries working after send

--- src/QueryList.py
+++ src/QueryList.py
@@ -1,22 +1,47 @@
 class QueryList:
+    '''
+    
+    '''
     __slots__ = 'list_of_queries'
 
     def __init__(self):
+        '''
+        
+        '''
         list_of_queries = []
 
     def get_list_of_queries(self):
+        '''
+        
+        :return: 
+        '''
         return self.list_of_queries
 
-    def add_query(self, query, product, table):
-        if query.upper() == "SELECT" and table:
+    def add_query(self, query, product=None, table=""):
+        '''
+        
+        :param query: 
+        :param product: 
+        :param table: 
+        :return: 
+        '''
+        if query.upper() == "SELECT" and table != "":
             query = "SELECT * from " + table + " WHERE id=" + product.pid + " and category=" + product.category + ";"
-        elif query.upper() == "INSERT" and table:
-            query = "INSERT INTO products(id, name, price, category) VALUES(1, 'Google Nexus', 650, 'Mobile');"
-        elif query.upper() == "DELETE":
-            query = "DELETE FROM " + table + " WHERE id=" + product.pid + ";"
-        self.list_of_queries.append(query)
+        elif query.upper() == "INSERT" and table != "":
+            query = "INSERT INTO " + table + "(id, name, price, category) VALUES(" + product.pid + " , '" + product.name + "', " + product.price + ", '" + product.category + "');"
+        elif query.upper() == "DELETE" and table == "":
+            query = "DELETE FROM cart WHERE product_id=" + product + ";"
+        print(query)
+        # self.list_of_queries.append(query)
 
     def remove_query(self, query, product, table):
+        '''
+        
+        :param query: 
+        :param product: 
+        :param table: 
+        :return: 
+        '''
         if query.upper() == "SELECT" and table:
             query = "SELECT * from " + table + " WHERE id=" + product.pid + " and category=" + product.category + ";"
         elif query.upper() == "INSERT" and table:
